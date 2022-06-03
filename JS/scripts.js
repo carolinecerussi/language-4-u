89dcffee0a5ba963efdc1deb5005138f6d9006ad
Caroline Cerussi <caroceru@gmail.com>
2022-06-03 14:04:36 -0400

    add function to scripts.js file and attempt effect on browser

---
+++ JS/scripts.js
@@ -0,0 +1,19 @@
+//business logic//
+
+
+
+
+//interface//
+$(document).ready(function() {
+
+    $("form#codeSurvey").submit(function(){
+        $("output#cSharp").show();
+
+    });
+
+}); 
+
+
+
+
+
