980754a29cb47d4144c045b3e8abf229cd607200
Caroline Cerussi <caroceru@gmail.com>
2022-06-03 18:56:10 -0400

    add new elements to html/js and attempt to try and get functions to work...

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,11 +2,17 @@
 
 
 //interface logic//
+const q1 = $("input:radio[name=q1]:checked").val();
+const q2 = $("input:radio[name=q2]:checked").val();
+const q3 = $("input:radio[name=q3]:checked").val();
+const q4 = $("input:radio[name=q4]:checked").val();
+const q5 = $("input:radio[name=q5]checked").val();
+const q6 = $("input:radio[name=q6]:checked").val();
 
-$(document).ready(function() {
-  $("form").submit(function() {
-    $(".result").show();
-  });
-});
+
+
+
+
+
 
 $(document).ready
