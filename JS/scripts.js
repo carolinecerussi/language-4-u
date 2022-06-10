000ba07e4506fc8dcae7867098a1977c6698e3b6
caroline cerussi <caroceru@gmail.com>
2022-06-09 20:21:04 -0700

    add functioning JS code to create an output showing IMG when form submitted

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,17 +1,16 @@
-//business logic//
-
-
-//interface logic//
-const q1 = $("input:radio[name=q1]:checked").val();
-const q2 = $("input:radio[name=q2]:checked").val();
-const q3 = $("input:radio[name=q3]:checked").val();
-const q4 = $("input:radio[name=q4]:checked").val();
-const q5 = $("input:radio[name=q5]checked").val();
-const q6 = $("input:radio[name=q6]:checked").val();
-
-
 $(document).ready(function() {
-    
-
-}
+  $("#form1").submit(function(event) {
+    const q1 = $("input:radio[name=q1]:checked").val();
+    const q2 = $("input:radio[name=q2]:checked").val();
+    const q3 = $("input:radio[name=q3]:checked").val();
+    const q4 = $("input:radio[name=q4]:checked").val();
+    const q5 = $("input:radio[name=q5]checked").val();
+    const q6 = $("input:radio[name=q6]:checked").val();
+    if (q1 === "a") {
+      $(".formContainer").hide()
+      $(".ruby").show()
+    }
+    event.preventDefault()
+   })
+});
   
