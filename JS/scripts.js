52c877321d142eb96d063c3a5d389df84ed11905
Caroline Cerussi <caroceru@gmail.com>
2023-01-13 18:04:43 -0800

    rewrite scripts file with updated user interface logic and calls since last year's versioning

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,74 +1,25 @@
-function addTotal(currentTotal,newTotal) {
-  return currentTotal + newTotal;
+// User Interface Logic
+
+function hideResults() {
+  document.getElementById("react").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("cSharp").setAttribute("class", "hidden");
+  document.getElementById("javaScript").setAttribute("class", "hidden");
 }
 
-let currentTotal = 0;
- let checkedAnswerVal = (document.getElementsByName('quizForm').checked);
- let newTotal = currentTotal + checkedAnswerVal;
-console.log(checkedAnswerVal);
-console.log(newTotal);
+window.onload = function() {
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResults();
+    const age = parseInt(document.querySelector("input#age").value);
+    const height = parseInt(document.querySelector("input#height").value);
+     if (age === 1 && height >= 60) {
+      document.getElementById("react").removeAttribute("class");
+      // goes to swings, removes class=hidden attribute, which removes the css styles code saying .hidden =display: none//
+      document.getElementById("python").removeAttribute("class");
+      document.getElementById("cSharp").removeAttribute("class");
+      document.getElementById("javaScript").removeAttribute("class");
 
-
-
-
-// document.getElementById('answer1').value;
-// document.getElementById('answer2').value;
-// document.getElementById('answer3').value;
-
-
-function quizTotal() {
-if (selectedAnswer[checked] && selectedAnswer === answerWorthArray[1] ) {
-  return currentTotal;
-} else if  (selectedAnswer[checked] = true && selectedAnswer === answerWorthArray[2] ) {
-  return currentTotal;
-} else if (selectedAnswer[checked] = true && selectedAnswer === answerWorthArray[3] ) {
-  return currentTotal;
-
+    } 
+  }
 }
-  console.log(selectedAnswer);
-quizTotal();
-}
-
-
-
-
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
-function findOutcome(selectedAnswer){
-
-  return selectedAnswer;
-}
-
-function add(number1, number2) {
-  return number1 + number2;
-}
-
-
-
-
-
-// $(document).ready(function() {
-//   $("#form1").submit(function(event) {
-//     const q1 = $("input:radio[name=q1]:checked").val();
-//     const q2 = $("input:radio[name=q2]:checked").val();
-//     const q3 = $("input:radio[name=q3]:checked").val();
-//     const q4 = $("input:radio[name=q4]:checked").val();
-//     const q5 = $("input:radio[name=q5]checked").val();
-//     const q6 = $("input:radio[name=q6]:checked").val();
-//     if (q1 === "a") {
-//       $(".formContainer").hide()
-//       $(".cSharp").show()
-//     } if (q1 === "b") {
-//       $(".formContainer").hide()
-//       $(".ruby").show()
-//     } if (q1 === "c") {
-//       $(".formContainer").hide()
-//       $(".python").show()
-//     }
-//     event.preventDefault()
-//    })
-// });
-  
-
