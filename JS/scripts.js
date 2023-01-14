a3b3bf208eb4b3c147f4c17c1c0ae62fe22a4df6
Caroline Cerussi <caroceru@gmail.com>
2023-01-13 19:08:37 -0800

    format radio button with fieldsets

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,12 @@
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  let resetBtn = document.querySelector("button#reset");
+  let quiz = document.querySelector("div#quiz");
+
+})
+
+
+
 // User Interface Logic
 
 function hideResults() {
@@ -11,14 +20,15 @@ window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
-    const age = parseInt(document.querySelector("input#age").value);
-    const height = parseInt(document.querySelector("input#height").value);
-     if (age === 1 && height >= 60) {
+    const questionOne = parseInt(document.querySelector("input#answerOne").checked.value);
+    const questionTwo = parseInt(document.querySelector("input#answerTwo").checked.value);
+    const questionThree = parseInt(document.querySelector("input#answerThree").checked.value);
+     if (questionOne === "1" && questionTwo === "1" && questionThree==="1") {
       document.getElementById("react").removeAttribute("class");
       // goes to swings, removes class=hidden attribute, which removes the css styles code saying .hidden =display: none//
-      document.getElementById("python").removeAttribute("class");
-      document.getElementById("cSharp").removeAttribute("class");
-      document.getElementById("javaScript").removeAttribute("class");
+      // document.getElementById("python").removeAttribute("class");
+      // document.getElementById("cSharp").removeAttribute("class");
+      // document.getElementById("javaScript").removeAttribute("class");
 
     } 
   }
