31f1c9409bef0fc5b5dd3767f773f709dd0dc141
Caroline Cerussi <caroceru@gmail.com>
2023-01-17 00:42:47 -0800

    get window to alert the reaction

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,10 +6,17 @@
 
 // // // User Interface Logic
 
-function hideResults() {
- document.getElementById("react").setAttribute("class", "hidden");
- document.getElementById("python").setAttribute("class", "hidden");
- document.getElementById("cSharp").setAttribute("class", "hidden");
+function hideResults(r) {
+document.getElementById("react").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("cSharp").setAttribute("class", "hidden");
+  document.getElementById("javaScript").setAttribute("class", "hidden");
+}
+function showResults() {
+  document.getElementById("react").setAttribute("class", "shown");
+  document.getElementById("python").setAttribute("class", "shown");
+  document.getElementById("cSharp").setAttribute("class", "shown");
+  document.getElementById("javaScript").setAttribute("class", "shown");
 }
 
 
@@ -17,63 +24,35 @@ window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
+
+    showResults();
       let answerChosen1 = document.querySelector("input[name='answerOne']:checked").value;
       let answerChosen2 = document.querySelector("input[name='answerTwo']:checked").value;
       let answerChosen3 = document.querySelector("input[name='answerThree']:checked").value;
 
 const answersTotal = (parseInt(answerChosen1) + parseInt(answerChosen2) + parseInt(answerChosen3));
-
+console.log(answersTotal);
 
       // goes to swings, removes class=hidden attribute, which removes the css styles code saying .hidden =display: none//
     
-
-      let hiddenOutcome = document.body.querySelector(
-          "hidden");
+  
       if (answersTotal === 3) {
-      let  hiddenOutcome = window.document.querySelector("div#react");
-              window.alert(hiddenOutcome.getAttribute('id'));
-              showResults();
-         }
+      let  hiddenOutcome = window.document.querySelector("p#react").getAttribute("id");
+          window.alert(hiddenOutcome);
+           }
       else  if  (answersTotal > 3 && answersTotal < 9) {
-          let  hiddenOutcome = window.document.querySelector("div#python")
-              console.log(hiddenOutcome.getAttribute('id'));
-              window.alert(hiddenOutcome.getAttribute('id'));
-              showResults();
-
+          let  hiddenOutcome = window.document.querySelector("p#python").getAttribute("id");
+          window.alert(hiddenOutcome);
       }
     else  if (answersTotal === 9)  {
-            let    hiddenOutcome = window.document.querySelector("div#cSharp");
-                  window.alert(hiddenOutcome.getAttribute('id'));
-              showResults();
+            let    hiddenOutcome = window.document.querySelector("p#cSharp").getAttribute("id");
+          window.alert(hiddenOutcome);
     }
 else {
   return window.alert("error");
 }
 
-
-function showResults() {
-  if (hiddenOutcome === "react") { 
-    document.querySelector("div#react").setAttribute("class", "reactShown");
-} else if 
-  (hiddenOutcome === "python") {
-    document.querySelector("div#python").setAttribute("class", "pythonShown");
-} else {
-      document.querySelector("div#cSharp").setAttribute("class", "cSharpShown");
+}  
 }
-};
 
 
-function showResults() {
-  if (hiddenOutcome === "react") { 
-    document.getElementById("react").setAttribute("class", "reactShown");
-} else if 
-  (hiddenOutcome === "python") {
-    document.getElementById("python").setAttribute("class", "pythonShown");
-} else {
-      document.getElementById("cSharp").setAttribute("class", "cSharpShown");
-}
-};
-showResults();
-
-}
-}
